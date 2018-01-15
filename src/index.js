a47aa548153d098000460109fd63d01712ee1419
aleksandrajagodzinska <aleksandra.berry01@gmail.com>
2018-01-15 21:06:23 +0100

    Interactive component

--- src/index.js
+++ src/index.js
@@ -4,20 +4,27 @@ import './index.css';
 
 
 class Square extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            value: null,
+        };
+    }
     render() {
         return (
-            <button className="square">
-            {this.props.value}
-    </button>
-    );
+            <button className="square"
+                    onClick={() => alert('click')}>
+                {this.props.value}
+            </button>
+        );
     }
 }
-
 
 
 class Board extends React.Component {
     renderSquare(i) {
         return
-    <Square
-        value={i} />;
+        <Square
+            value={i}/>;
     }
+}
